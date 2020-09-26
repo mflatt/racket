41585e3aab5ec19d099628dfe591825ba30cf2d7
Matthew Flatt <mflatt@racket-lang.org>
2020-09-26 17:32:40 -0600

    Chez Scheme GC: keep per-thread allocation more separate
    When collecting and a thread doesn't get its own sweeper thread, then
    instead of moving its segments to another thread, keep the thread
    separate by making each sweeper deal with multiple threads.

--- racket/src/ChezScheme/c/thread.c
+++ racket/src/ChezScheme/c/thread.c
@@ -159,12 +159,15 @@ ptr S_create_thread_object(who, p_tc) const char *who; ptr p_tc; {
   LZ4OUTBUFFER(tc) = 0;
 
   tgc->sweeper = main_sweeper_index;
-  tgc->will_be_sweeper = main_sweeper_index;
-  tgc->will_be_sweeper = main_sweeper_index;
   tgc->remote_range_start = (ptr)(uptr)-1;
   tgc->remote_range_end = (ptr)0;
   tgc->pending_ephemerons = (ptr)0;
-
+  tgc->ranges_to_send = NULL;
+  tgc->ranges_received = NULL;
+  for (i = 0; i < (int)DIRTY_SEGMENT_LISTS; i++)
+    tgc->dirty_segments[i] = NULL;
+  tgc->thread = (ptr)0;
+  
   tc_mutex_release();
 
   return thread;
