35dac723120038d4c19a17b857613537e84126a6
Matthew Flatt <mflatt@racket-lang.org>
2022-01-28 17:19:52 -0700

    fix endian swap pb-adr decode

--- racket/src/ChezScheme/c/fasl.c
+++ racket/src/ChezScheme/c/fasl.c
@@ -2038,7 +2038,7 @@ static void swap_code_endian(octet *code, uptr len)
 
       if (a == pb_adr) {
         /* delta can be negative for a mvlet-error reinstall of the return address */
-        iptr delta = (((iptr)d << (ptr_bits - 8)) >> (ptr_bits - 20)) + ((iptr)c << 4) + (b >> 4);
+        iptr delta = 4*((((iptr)d << (ptr_bits - 8)) >> (ptr_bits - 20)) + ((iptr)c << 4) + (b >> 4));
         if (delta > 0) {
           /* after a few more instructions, we'll hit
              a header where 64-bit values needs to be
@@ -2049,8 +2049,6 @@ static void swap_code_endian(octet *code, uptr len)
 
 	  if ((uptr)delta > len)
 	    S_error_abort("swap endian: delta goes past end");
-	  if (delta & 0x3)
-	    S_error_abort("swap endian: delta is not a multiple of 4");
 
           if (after_rpheader[-8] & 0x1)
             header_size = size_rp_compact_header;
