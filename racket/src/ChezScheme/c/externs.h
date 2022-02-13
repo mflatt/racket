dddedc544bd90aa4fceb28842e1731f347cc41a1
Matthew Flatt <mflatt@racket-lang.org>
2022-02-13 13:33:12 -0700

    fixup revert i/o

--- racket/src/ChezScheme/c/externs.h
+++ racket/src/ChezScheme/c/externs.h
@@ -208,7 +208,7 @@ extern IBOOL S_file_existsp PROTO((const char *inpath, IBOOL followp));
 extern IBOOL S_file_regularp PROTO((const char *inpath, IBOOL followp));
 extern IBOOL S_file_directoryp PROTO((const char *inpath, IBOOL followp));
 extern IBOOL S_file_symbolic_linkp PROTO((const char *inpath));
-#if defined(WIN32) && !defined(PORTABLE_BYTECODE)
+#ifdef WIN32
 extern ptr S_find_files PROTO((const char *wildpath));
 #else
 extern ptr S_directory_list PROTO((const char *inpath));
