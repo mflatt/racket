8ba14b869b0f76a1f77882d5556c69a4d08dfa6c
Matthew Flatt <mflatt@racket-lang.org>
2022-02-14 13:24:22 -0700

    boot.bat

---
+++ racket/src/ChezScheme/s/boot.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+
+set M=%1
+set H=%2
+
+set WORKAREA=%M%
+
+if "%WORKAREA%"=="" goto needargument
+
+if "%H%"=="" H=pb
+
+xcopy /s /i /d /y s xc-%WORKAREA%\s
+xcopy /s /i /d /y s xc-%WORKAREA%\nanopass
+xcopy /s /i /d /y s xc-%WORKAREA%\unicode
+
+cd xc-%WORKAREA%\s
+..\..\%H%\bin\%H%\scheme.exe --script make-xpatch.ss %M% patch
+..\..\%H%\bin\%H%\scheme.exe --script make-xpatch.ss %M% build
+
+xcopy /s /s /d /y xc-%WORKAREA%\boot\%M% boot\%M%
+
+goto donebuilding
+
+:needargument
+
+echo Please supply the machine name as an argument
+exit /B 1
+
+:donebuilding
