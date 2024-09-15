707c73fadf992bd73fdf1b35295cd6ac67fd7ec8
Matthew Flatt <mflatt@racket-lang.org>
2024-09-15 07:39:01 -0600

    parallel GC again

--- racket/src/ChezScheme/c/gcwrapper.c
+++ racket/src/ChezScheme/c/gcwrapper.c
@@ -1324,7 +1324,7 @@ ptr S_gc(ptr tc, IGEN max_cg, IGEN min_tg, IGEN max_tg, ptr count_roots) {
       || (count_roots != Sfalse)) {
     if (S_G.enable_object_backreferences) min_tg = max_tg;
     return S_gc_oce(tc, max_cg, min_tg, max_tg, count_roots);
-#if defined(XX_PTHREADS)
+#if defined(PTHREADS)
   } else if ((S_collect_waiting_threads != 0)
              || (Spairp(S_threads)
                  && Spairp(Scdr(S_threads))
