dbc43c04ede036255d2e610e7446f90205500147
Matthew Flatt <mflatt@racket-lang.org>
2026-07-07 13:22:33 -0700

    Windows: update native-library builds for x86_64 and Arm64
    The actual build scripts are now at
    
     https://github.com/ndykman/racket-native-libs
    
    and the updates here bump versions and add `ffi/vcruntime`.

---
+++ racket/collects/ffi/vcruntime.rkt
@@ -0,0 +1,12 @@
+#lang racket/base
+(require ffi/unsafe/runtime-lib)
+
+;; The intent of this module is just to load some Windows DLLs and
+;; pull them along in standard executables, and not to export the
+;; libraries or bindings
+
+(define-runtime-lib vcruntime
+  [(and windows 64)
+   (so "vcruntime140.dll")
+   (so "vcruntime140_1.dll")]
+  [else #f])
