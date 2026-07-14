mflatt/ChezScheme

avoid quadratics in call-live information
Using a tree representation enables sharing to avoid a quadratic-sized
compiled form and intermediate quadtraic-time/space representations
for a program like this one, where there are N calls each with an
average of N/2 live variables:

 (define vars
   (let loop ([i 10000])
     (cond
      [(zero? i) '()]
      [else (cons (gensym) (loop (sub1 i)))])))

 (time
  (begin
   (compile
    `(lambda ,vars
       ,@(map (lambda (v) `(,v)) vars)))
   (void)))

Keeping the variables in tree form (since they're already collected
that way) and memoizing reduces on the tree allows sharing to be
constructed and preserved. The tree approach persists even to the
runtime mask for live variables.

s/cmacros.ss
@@ -357,7 +357,7 @@
 ;; ---------------------------------------------------------------------
 ;; Version and machine types:
 
-(define-constant scheme-version #x0a010001)
+(define-constant scheme-version #x0a010002)
 
 (define-syntax define-machine-types
   (lambda (x)
@@ -1748,7 +1748,7 @@
 
 (define-primitive-structure-disps rp-header type-untyped
   ([uptr mv-return-address]
-   [ptr livemask]
+   [ptr livemask] ; a fixnum or (cons size tree)
    [uptr toplink]
    [iptr frame-size])) ; low bit is 0 to distinguish from a `rp-compact-header`
 (define-constant return-address-mv-return-address-disp

mflatt/racket

racket/src/ChezScheme/s/cmacros.ss
@@ -357,7 +357,7 @@
 ;; ---------------------------------------------------------------------
 ;; Version and machine types:
 
-(define-constant scheme-version #x0a050001)
+(define-constant scheme-version #x0a050002)
 
 (define-syntax define-machine-types
   (lambda (x)
@@ -1756,7 +1756,7 @@
 
 (define-primitive-structure-disps rp-header type-untyped
   ([uptr mv-return-address]
-   [ptr livemask]
+   [ptr livemask] ; a fixnum or (cons size tree)
    [uptr toplink]
    [iptr frame-size])) ; low bit is 0 to distinguish from a `rp-compact-header`
 (define-constant return-address-mv-return-address-disp